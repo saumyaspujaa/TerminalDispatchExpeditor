83ec2e5236c8a4a313da91f7e1afbdf084a34164
zaid kamil <xaidmetamorphos@gmail.com>
2020-11-01 22:07:47 +0530

    remove fab in main

--- app/src/main/java/com/digipodium/tde/MainActivity.java
+++ app/src/main/java/com/digipodium/tde/MainActivity.java
@@ -33,14 +33,7 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
